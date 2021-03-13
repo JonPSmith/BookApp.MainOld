1c3cb67009fdfc4726991a15e5e8c5ce4d98e1e1
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-03-13 11:16:08 +0000

    Add project files.

---
+++ MonMon.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonMon.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
