e2c27bcfe7106283b950422d0f6118b122663c7a
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-03-31 13:50:34 +0100

    Added parts of the Order bounded context

---
+++ BookApp.Orders.BizLogic/Orders/IPlaceOrderBizLogic.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using BookApp.Orders.Domain;
+using StatusGeneric;
+
+namespace BookApp.Orders.BizLogic.Orders
+{
+    public interface IPlaceOrderBizLogic
+    {
+        Task<IStatusGeneric<Order>> CreateOrderAndSaveAsync(PlaceOrderInDto dto);
+    }
+}
